26e6d6537cefcc1fd72c874d8c3d4b4eb6d3bda1
nikhil-garg-192 <nikhilgarg192@rediffmail.com>
2024-08-29 22:58:01 +0530

    remark added

--- count_occurance.py
+++ count_occurance.py
@@ -5,7 +5,7 @@ myList.sort()
 print(myList)
 cntr=0
 for i in range(len(myList)):
-  if i==0 or (i!=0 and myList[i]!=myList[i-1]):
+  if i==0 or (i!=0 and myList[i]!=myList[i-1]):  #condition to check, counted valus should not be checked again
     print(f"{myList[i]}  Came  {myList.count(myList[i])} times")
   
   
